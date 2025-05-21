4c255e7fec5340eb1dad00e4500ea3f3656ca8ee
maha <maridayya@gmail.com>
2025-05-21 13:26:49 +0530

    roboshop into scripting

--- 01-roboshop.sh
+++ 01-roboshop.sh
@@ -27,24 +27,24 @@ do
         echo "$instance : $IP"
         RECORD_NAME=$instance.$DOMAIN_NAME
     fi
-    aws route53 change-resource-record-sets  --hosted-zone-id "$ZONE_ID"  --change-batch {
-     "Comment":  "Auto update DNS for $instance",
-     "Changes ": [
-      {
-         "Action":  "UPSERT",
-         "ResourceRecordSet": {
-           "Name":  "$RECORD_NAME",
-           "Type":  "A ",
-           "TTL": 1,
-           "ResourceRecords": [
-            {
-               "Value":  "$IP"
-            }
-          ]
-        }
+    aws route53 change-resource-record-sets --hosted-zone-id "$ZONE_ID" --change-batch '{
+  "Comment": "Auto update DNS for '"$instance"'",
+  "Changes": [
+    {
+      "Action": "UPSERT",
+      "ResourceRecordSet": {
+        "Name": "'"$RECORD_NAME"'",
+        "Type": "A",
+        "TTL": 1,
+        "ResourceRecords": [
+          {
+            "Value": "'"$IP"'"
+          }
+        ]
       }
-    ]
-  }
+    }
+  ]
+}'
 
 
 done
