be5092e6a7bc6dcc7eba2e216c408bb7bbff92fb
maha <maridayya@gmail.com>
2025-05-21 12:09:51 +0530

    roboshop into scripting

--- 01-roboshop.sh
+++ 01-roboshop.sh
@@ -16,9 +16,9 @@ do
 # search in google for script to create instances in aws cli
 #modify the script based on ur needs
 #here i dont want key pair, subnet etc so removed those portions and taken which is required for me
-    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-03de6c7ee76a1f5a3 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --query "Instances[*].InstanceId" --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-03de6c7ee76a1f5a3 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name, Value= $instance}]' --query "Instances[*].InstanceId" --output text)
     
-    if ( $instance -eq frontend )
+    if ( $instance -eq "frontend" )
     then
         IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
         echo " $instance: $IP"
