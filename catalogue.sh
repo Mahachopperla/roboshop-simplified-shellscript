28e13fc06d1f44fd2d7f0fa67cc5318c0259ff4f
maha <maridayya@gmail.com>
2025-05-22 18:19:11 +0530

    mongodb component

---
+++ catalogue.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+#few lines of code is common in all components(colr setting, log-file creation to store logs,checking user using root access or not)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+LOG_FOLDER="/var/log/roboshop-shellpractice" 
+FILE_NAME=$(echo $0 | cut -d "." -f1) 
+LOG_FILE="$LOG_FOLDER/$FILE_NAME.log"
+mkdir -p $LOG_FOLDER
+SCRIPT_LOCATION=$PWD
+
+echo "This script is getting executed at : $(date)" &>> $LOG_FILE 
+USERID=$(id -u)  #user id of root user will be 0
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:$N $Y please run command with root access to execute succesfully$N " | tee -a $LOG_FILE
+    exit 1
+fi
+
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+        then
+            echo -e " $G  $2 is successfull $N" | tee -a $LOG_FILE
+        else
+            echo -e " $R  $2 is failed $N" | tee -a $LOG_FILE
+            exit 1
+    fi
+}
+
+dnf list installed nodejs &>> $LOG_FILE 
+
+if [ $? -ne 0 ]  
+then
+    echo -e "$Y installing Nodejs.......$N" | tee -a $LOG_FILE
+    dnf module disable nodejs -y &>> $LOG_FILE
+    dnf module enable nodejs:20 -y &>> $LOG_FILE
+    VALIDATE $? "enabling nodejs:20"
+
+    dnf install nodejs -y &>> $LOG_FILE
+    VALIDATE $? "installation of nodejs:20"  
+
+else
+    echo -e "$G Nodejs is already installed... nothing to do$N" | tee -a $LOG_FILE
+fi
+
+id username &>> $LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo " user not there proceeding to create "
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+else
+    echo " user already existing so skipping user creation"
+fi
+
+mkdir -p /app # creates dir if not existing if existing it skips creation without giving error
+
+cd /app
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+rm -rf /app/*
+cd /app
+unzip /tmp/catalogue.zip > /app
+npm install 
+VALIDATE $? "build tool installed packages"
+
+cp $SCRIPT_LOCATION/catalogue.service /etc/systemd/system/catalogue.service
+VALIDATE $? "systemctl service file copying"
+
+systemctl daemon-reload
+systemctl enable catalogue 
+systemctl start catalogue
+
+cp $SCRIPT_LOCATION/mongodb.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-mongosh -y 
+VALIDATE $? "installation of mongo client "
+
+mongosh --host mongodb.robotshop.site </app/db/master-data.js
+
+
+
+
+
+
+
+
+
