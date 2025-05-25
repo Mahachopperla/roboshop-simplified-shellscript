a23ca2ea0d0b5bfe5c6a2c9416971ce4d3dcc922
maha <maridayya@gmail.com>
2025-05-25 10:35:42 +0530

    simplified

---
+++ script1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo " Hi this is script 1"
+echo " PID of current script : $$ "
+
+./script2.sh
---
+++ script2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "helloo this is script2"
+echo " PID of current script : $$ "
