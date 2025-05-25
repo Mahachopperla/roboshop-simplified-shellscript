b27fd207f42b1292fb3be4af38183b3a8b5bc5c1
maha <maridayya@gmail.com>
2025-05-25 19:58:32 +0530

    scirpt calling from other script

--- redis.sh
+++ redis.sh
@@ -1,38 +1,11 @@
 #!/bin/bash
 
 #few lines of code is common in all components(colr setting, log-file creation to store logs,checking user using root access or not)
-
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-START_TIME=$(date +%s)
-LOG_FOLDER="/var/log/roboshop-shellpractice" 
-FILE_NAME=$(echo $0 | cut -d "." -f1) 
-LOG_FILE="$LOG_FOLDER/$FILE_NAME.log"
-mkdir -p $LOG_FOLDER
-SCRIPT_LOCATION=$PWD
-
-echo "This script is getting executed at : $(date)" &>> $LOG_FILE 
-USERID=$(id -u)  #user id of root user will be 0
-
-if [ $USERID -ne 0 ]
-then
-    echo -e "$R ERROR:$N $Y please run command with root access to execute succesfully$N " | tee -a $LOG_FILE
-    exit 1
-fi
+. ./common.sh
 
 
-VALIDATE(){
-    if [ $1 -eq 0 ]
-        then
-            echo -e " $G  $2 is successfull $N" | tee -a $LOG_FILE
-        else
-            echo -e " $R  $2 is failed $N" | tee -a $LOG_FILE
-            exit 1
-    fi
-}
+CHECK_ROOT
+
 #whatever location we are currently in, we need to copy mongodb.repo script from roboshop-shell-script dir so in starting of script only we are assigning it's value to script location variable
 
 dnf module disable redis -y &>> $LOG_FILE
@@ -57,6 +30,4 @@ VALIDATE $? "redis service start"
 systemctl restart redis &>> $LOG_FILE
 VALIDATE $? "redis service restarted"
 
-END_TIME=$(date +%s)
-TIME_TAKEN=$(($END_TIME - $START_TIME))
-echo "time taken to execute script is $TIME_TAKEN"
+TIME_TAKEN
