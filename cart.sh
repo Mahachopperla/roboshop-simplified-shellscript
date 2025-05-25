394ad2a6cf155da2c2eeb85cd1b1abb880669947
maha <maridayya@gmail.com>
2025-05-25 21:00:38 +0530

    scirpt calling from other script

--- cart.sh
+++ cart.sh
@@ -3,14 +3,14 @@
 #few lines of code is common in all components(colr setting, log-file creation to store logs,checking user using root access or not)
 . ./common.sh
 
-
+APP_NAME=cart
 CHECK_ROOT
 
 APP_SETUP
 NODEJS_SETUP
 
 USER_SETUP
-APP_NAME=cart
+
 
 
 
