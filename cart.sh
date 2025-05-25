4f4c63aaeb425d7332733970bfc6eb476e6e1877
maha <maridayya@gmail.com>
2025-05-25 09:19:56 +0530

    frontend component

--- cart.sh
+++ cart.sh
@@ -7,7 +7,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-
+START_TIME=$(date +%s)
 LOG_FOLDER="/var/log/roboshop-shellpractice" 
 FILE_NAME=$(echo $0 | cut -d "." -f1) 
 LOG_FILE="$LOG_FOLDER/$FILE_NAME.log"
@@ -81,3 +81,6 @@ VALIDATE $? "enabling cart"
 systemctl start cart
 VALIDATE $? "starting cart.service"
 
+END_TIME=$(date +%s)
+TIME_TAKEN=$($END_TIME-$START_TIME)
+echo "time taken to execute script is $TIME_TAKEN"
