073ed8b5bb3c32f99dc357c8feaf2644a25ff0e9
maha <maridayya@gmail.com>
2025-05-25 09:18:47 +0530

    frontend component

--- frontend.sh
+++ frontend.sh
@@ -7,7 +7,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-
+START_TIME=$(date +%s)
 LOG_FOLDER="/var/log/roboshop-shellpractice" 
 FILE_NAME=$(echo $0 | cut -d "." -f1) 
 LOG_FILE="$LOG_FOLDER/$FILE_NAME.log"
@@ -69,3 +69,6 @@ VALIDATE $? " nginx.conf file updation"
 systemctl restart nginx &>> $LOG_FILE
 VALIDATE $? "service restart "
 
+END_TIME=$(date +%s)
+TIME_TAKEN=$($END_TIME-$START_TIME)
+echo "time taken to execute script is $TIME_TAKEN"
