3a59d8a3c7bf88fb49dcc4d92edd5b92e30b56fa
maha <maridayya@gmail.com>
2025-05-25 09:21:16 +0530

    frontend component

--- catalogue.sh
+++ catalogue.sh
@@ -7,7 +7,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-
+START_TIME=$(date +%s)
 LOG_FOLDER="/var/log/roboshop-shellpractice" 
 FILE_NAME=$(echo $0 | cut -d "." -f1) 
 LOG_FILE="$LOG_FOLDER/$FILE_NAME.log"
@@ -93,7 +93,9 @@ else
     echo -e "Data is already loaded ... $Y SKIPPING $N"
 fi
 
-
+END_TIME=$(date +%s)
+TIME_TAKEN=$($END_TIME-$START_TIME)
+echo "time taken to execute script is $TIME_TAKEN"
 
 
 
